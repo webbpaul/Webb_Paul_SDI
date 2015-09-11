8adf18aa44fa2089d51279a78b2f637075c02772
webbpaul <pawebb@fullsail.edu>
2015-09-11 17:13:50 -0400

    Asked the user how often they do their favorite activity.
    Asked the user for how often they do their favorite activity and then
    logged the activity.

--- Webb_Paul_Expressions_Assignment/js/expressionsAssignment.js
+++ Webb_Paul_Expressions_Assignment/js/expressionsAssignment.js
@@ -8,10 +8,17 @@
 //alert("Test to see if connected");
 
 //Using prompt to ask the user for their name
-var name = prompt("We are trying to figure out how often you do your favorite activity. \nPlease endter your name.");
+var name = prompt("We are trying to figure out how often you do your favorite activity. \nPlease enter your name.");
 
 //Using the variable "name" to alert the user and say hello.
 alert("Hello " +name+ ".");
 
 //Asking the user what their favorite activity is
 var favoriteActivity = prompt("What is your favorite activity " +name+ "?");
+
+//Asking the user how many times a month they do this activity.
+var howOftenWeekly = prompt("How many times a week do you do your favorite activity?");
+
+//console.log the user's favorite activity.  (Checked to see if the favorite activity was logged.)
+console.log(howOftenWeekly);
+
