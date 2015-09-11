06f5f75ecd658d66b62c726b43b2d17741452425
webbpaul <pawebb@fullsail.edu>
2015-09-11 16:43:31 -0400

    Asked for user's name.
    Asked for the users name using prompt, and then used the variable name
    to alert the user by saying hello.

--- Webb_Paul_Expressions_Assignment/js/expressionsAssignment.js
+++ Webb_Paul_Expressions_Assignment/js/expressionsAssignment.js
@@ -1,8 +1,14 @@
 /*
-Paul Webb
-SDI Section 1
-Expressions Assignment
-9/11/15
+ Paul Webb
+ SDI Section 1
+ Expressions Assignment
+ 9/11/15
  */
 
-alert("Test to see if connected");
+//alert("Test to see if connected");
+
+//Using prompt to ask the user for their name
+var name = prompt("We are trying to figure out how often you do your favorite activity. \nPlease endter your name.");
+
+//Using the variable "name" to alert the user and say hello.
+alert("Hello " +name+ ".");
