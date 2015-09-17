526293ff7f87da79c5ccac6628bc33ebf5197a33
webbpaul <pawebb@fullsail.edu>
2015-09-16 21:07:12 -0400

    Asking user for information
    asked for users birth year to determine age.

--- Webb_Paul_Conditionals_Assignment/js/conditionalsAssignment.js
+++ Webb_Paul_Conditionals_Assignment/js/conditionalsAssignment.js
@@ -5,4 +5,17 @@ Conditionals Assignment
 9/16/15
  */
 
-//alert("Test to see if connected.");
+//Asking for the users name.
+var name = prompt("Hello. Let's figure out if you're old enough to enter the concert. \nPlease enter your name.");
+
+//Asking for the users birth year.
+var yearBorn = prompt("Hello " +name+ ".  What year were you born?")
+
+//Setting the variable current year to 2015
+var currentYear = 2015;
+
+//Figuring out the users age.
+var oldEnough= currentYear - yearBorn;
+
+//Putting the users age into the console. 
+console.log(oldEnough)
