0bba03532406ae6cde1b8f09cfe93064fdc9026f
webbpaul <pawebb@fullsail.edu>
2015-09-16 21:28:53 -0400

    Adding conditionals and more variables
    adding conditionals and more variables.

--- Webb_Paul_Conditionals_Assignment/js/conditionalsAssignment.js
+++ Webb_Paul_Conditionals_Assignment/js/conditionalsAssignment.js
@@ -8,14 +8,35 @@ Conditionals Assignment
 //Asking for the users name.
 var name = prompt("Hello. Let's figure out if you're old enough to enter the concert. \nPlease enter your name.");
 
+//Test to see if user left the prompt blank.
+if(name === ""){
+    //user left blank.  prompt again.
+    name = prompt("Please do not leave blank information. \nPlease enter your name.")
+}
+
 //Asking for the users birth year.
 var yearBorn = prompt("Hello " +name+ ".  What year were you born?")
+
+if(isNaN(yearBorn)){
+    //Making sure the user enters in a number
+    yearBorn = prompt("Please only enter numbers here. \nPlease enter the year you were born.")
+}
 
 //Setting the variable current year to 2015
 var currentYear = 2015;
 
 //Figuring out the users age.
-var oldEnough= currentYear - yearBorn;
+var age= currentYear - yearBorn;
 
-//Putting the users age into the console. 
-console.log(oldEnough)
+//Putting the users age into the console.
+console.log(age);
+
+//Setting the age to enter the concert to 18.
+var ageToEnter = 18;
+
+//Setting the age to enter with parent to 16.
+var ageWithParent = 16;
+
+//Setting the age to drink at the concert to 21.
+var ageToDrink = 21;
+
