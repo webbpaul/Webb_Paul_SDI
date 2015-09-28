f6701148053577dd67a27d6a08b529f1ff092d68
webbpaul <pawebb@fullsail.edu>
2015-09-27 22:57:56 -0400

    Setting up anonymous function
    Setting up anonymous function

--- Webb_Paul_Functions_Assignment/js/functions.js
+++ Webb_Paul_Functions_Assignment/js/functions.js
@@ -6,22 +6,22 @@ Functions Assignment
  */
 
 //Asking the user for the length of their box.
-var boxLength = prompt("We are trying to calculate the volume of a box. \nPlease enter the height of the box.")
+var boxLength = prompt("We are trying to calculate the volume of a box. \nPlease enter the height of the box.");
 
 //Asking the user for the width of their box
-var boxWidth = prompt("Please enter the width of the box.")
+var boxWidth = prompt("Please enter the width of the box.");
 
 //Asking the user for the height of their box
-var boxHeight = prompt("Please enter the height of the box.")
+var boxHeight = prompt("Please enter the height of the box.");
 
 //Calling the function
-var boxVolume = calcVolume(boxLength, boxWidth, boxHeight)
+var boxVolume = calcVolume(boxLength, boxWidth, boxHeight);
 
 //Sending the calculated volume to the console.
 console.log(boxVolume)
 
 //Alerting the user with the volume of their box.
-alert("The volume of your box is " + boxVolume + ".")
+alert("The volume of your box is " + boxVolume + ".");
 
 //Function for calculating volume.
 function calcVolume(length, width, height){ // parameters for the calculation.
@@ -30,4 +30,20 @@ function calcVolume(length, width, height){ // parameters for the calculation.
 }
 
 
+var side1 = prompt("Now lets find the perimeter of a trianlgle. \nPlease enter the length of side one of the triangle.");
 
+
+var side2 = prompt("Please enter the length of side two of the triangle.");
+
+
+var side3 = prompt("Please enter the length of side three of the triangle.");
+
+
+var calcPerimeter = function(firstSide, secondSide, thirdSide){
+    var perimeter = Number(firstSide) + Number(secondSide) + Number(thirdSide);
+    return perimeter
+}
+
+
+var p = calcPerimeter(side1, side2, side3)
+console.log(p)
