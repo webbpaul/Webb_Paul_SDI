db419b722e3d1dec10c7424f5a37965012e83205
webbpaul <pawebb@fullsail.edu>
2015-09-27 21:50:06 -0400

    Setting up Functions Assignment file
    Setting up Functions Assignment file

---
+++ Webb_Paul_Functions_Assignment/js/functions.js
@@ -0,0 +1,12 @@
+/*
+Paul Webb
+SDI Section 1
+Functions Assignment
+9-27-15
+ */
+
+var boxLength = prompt("We are trying to calculate the volume of a box. \nPlease enter the height of the box.")
+
+
+
+
