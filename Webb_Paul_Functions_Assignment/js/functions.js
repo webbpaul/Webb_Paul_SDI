190d9b45ac5a64e3d25c3e7f1b12fd7b4dc998df
webbpaul <pawebb@fullsail.edu>
2015-09-27 22:03:36 -0400

    setting up prompts to collect user info
    setting up prompts to collect user info

--- Webb_Paul_Functions_Assignment/js/functions.js
+++ Webb_Paul_Functions_Assignment/js/functions.js
@@ -8,5 +8,10 @@ Functions Assignment
 var boxLength = prompt("We are trying to calculate the volume of a box. \nPlease enter the height of the box.")
 
 
+var boxWidth = prompt("Please enter the width of the box.")
+
+
+var boxHeight = prompt("Please enter the height of the box.")
+
 
 
