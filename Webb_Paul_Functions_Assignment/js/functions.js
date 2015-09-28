ad263fa28d46a29ffb72edbfeb5911954978069c
webbpaul <pawebb@fullsail.edu>
2015-09-27 22:36:15 -0400

    Adding comments
    Adding comments

--- Webb_Paul_Functions_Assignment/js/functions.js
+++ Webb_Paul_Functions_Assignment/js/functions.js
@@ -5,25 +5,28 @@ Functions Assignment
 9-27-15
  */
 
+//Asking the user for the length of their box.
 var boxLength = prompt("We are trying to calculate the volume of a box. \nPlease enter the height of the box.")
 
-
+//Asking the user for the width of their box
 var boxWidth = prompt("Please enter the width of the box.")
 
-
+//Asking the user for the height of their box
 var boxHeight = prompt("Please enter the height of the box.")
 
-
+//Calling the function
 var boxVolume = calcVolume(boxLength, boxWidth, boxHeight)
 
-
+//Sending the calculated volume to the console.
 console.log(boxVolume)
+
+//Alerting the user with the volume of their box.
 alert("The volume of your box is " + boxVolume + ".")
 
-
-function calcVolume(l,w, h){
-    var volume = l * w * h;
-    return volume;
+//Function for calculating volume.
+function calcVolume(length, width, height){ // parameters for the calculation.
+    var volume = length * width * height;  // calculation for volume.
+    return volume; //returning the function.
 }
 
 
